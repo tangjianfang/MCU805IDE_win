dda563089fa6d69464eed929e1cea21c7cb551f9
MIS\mike <1255033066@qq.com>
2026-06-06 19:46:57 +0800

    从 git 跟踪中移除 build 目录
    - 移除 build/ 目录的所有文件跟踪 (1010 个文件)
    - build/ 目录已在 .gitignore 中忽略
    - 这些文件应该由 build_exe.bat 动态生成
    - 减少仓库大小，避免重复提交构建产物
    
    用户运行 build_exe.bat 时会自动生成 build/ 目录

--- build/libraries/tclx8.4/pkgIndex.tcl
+++
@@ -1,5 +0,0 @@
-# Package index for TclX @TCLX_FULL_VERSION@.
-#
-if {![package vsatisfies [package provide Tcl] 8.4]} { return }
-package ifneeded Tclx 8.4 \
-    [list load [file join $dir tclx84.dll] Tclx]
--- build/libraries/tdom/pkgIndex.tcl
+++
@@ -1,6 +0,0 @@
-#
-# Tcl package index file
-#
-package ifneeded tdom 0.9.3 \
-    "[list load [file join $dir tdom093.dll]];
-     [list source [file join $dir tdom.tcl]]"
